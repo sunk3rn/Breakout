73738b40c48cfac983aa97fa44f86382bac5066a
sunk3rn <kajakobliha@seznam.cz>
2023-11-21 23:06:31 +0100

    made the player block move

--- hra.c
+++ hra.c
@@ -20,19 +20,27 @@ int main()
         -1,
         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
     );
-    SDL_Event e;
+    SDL_Event event;
     bool quit = false;
     int pos = 100;
+    
+    int mouse_x = 0;
+    int mouse_y = 0;
 
     while (!quit)
     {
         // Dokud jsou k dispozici nějaké události, ukládej je do proměnné `e`
-        while (SDL_PollEvent(&e))
+        while (SDL_PollEvent(&event))
         {
             // Pokud došlo k uzavření okna, nastav proměnnou `quit` na `true`
-            if (e.type == SDL_QUIT)
+            if (event.type == SDL_QUIT)
             {
                 quit = true;
+            }
+            if(event.type == SDL_MOUSEMOTION) {
+                mouse_x =  event.motion.x;
+                if (mouse_x-50 <= 50) mouse_x = 80;
+                else if (mouse_x+50 >= 750) mouse_x = 720;
             }
         }
 
@@ -43,12 +51,10 @@ int main()
         SDL_RenderClear(renderer);
 
         // Nastavení barvy na červenou
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 128, 128, 255);
 
-        // Vykreslení čáry
-        SDL_RenderDrawLine(renderer, pos, pos, pos + 100, pos + 100);
-
-        pos++;
+        SDL_Rect rectToDraw = {mouse_x-50,550,100,20}; //x_start, y_start, width, height
+        SDL_RenderFillRect(renderer, &rectToDraw);
 
         // Zobrazení vykreslených prvků na obrazovku
         SDL_RenderPresent(renderer);
@@ -60,6 +66,3 @@ int main()
 
     return 0;
 }
-
-
-
