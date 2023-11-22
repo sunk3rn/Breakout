d6b5772a0a1aa4244f8b0ee45c6a502d09f3f922
sunk3rn <kajakobliha@seznam.cz>
2023-11-22 22:55:48 +0100

    added keyboard controls

--- hra.c
+++ hra.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+    bool keyboard = true;
     SDL_Init(SDL_INIT_VIDEO);   // Inicializace SDL
 
     // Vytvoření okna
@@ -14,17 +15,18 @@ int main()
         600,                // Výška
         SDL_WINDOW_SHOWN    // Okno se má po vytvoření rovnou zobrazit
     );
-// Vytvoření kreslítka
+    SDL_SetWindowTitle(window,"Breakout");
+    // Vytvoření kreslítka
     SDL_Renderer* renderer = SDL_CreateRenderer(
         window,
         -1,
         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
     );
+
     SDL_Event event;
     bool quit = false;
-    int pos = 100;
-    
-    int mouse_x = 0;
+
+    int mouse_x = 200;
     int mouse_y = 0;
 
     while (!quit)
@@ -37,10 +39,27 @@ int main()
             {
                 quit = true;
             }
-            if(event.type == SDL_MOUSEMOTION) {
+            if(event.type == SDL_MOUSEMOTION && !keyboard) {
                 mouse_x =  event.motion.x;
                 if (mouse_x-50 <= 50) mouse_x = 80;
                 else if (mouse_x+50 >= 750) mouse_x = 720;
+            }
+            if (SDL_KEYDOWN && keyboard) {
+                switch (event.key.keysym.sym)
+                {
+                case SDLK_LEFT:
+                    mouse_x -=  10;
+                    if (mouse_x-50 <= 20) mouse_x = 80;
+                    break;
+                
+                case SDLK_RIGHT:
+                    mouse_x +=  10;
+                    if (mouse_x+50 >= 780) mouse_x = 720;
+                    break;
+                
+                default:
+                    break;
+                }
             }
         }
 
@@ -55,6 +74,7 @@ int main()
 
         SDL_Rect rectToDraw = {mouse_x-50,550,100,20}; //x_start, y_start, width, height
         SDL_RenderFillRect(renderer, &rectToDraw);
+         printf("mousex:%d\n",mouse_x);
 
         // Zobrazení vykreslených prvků na obrazovku
         SDL_RenderPresent(renderer);
