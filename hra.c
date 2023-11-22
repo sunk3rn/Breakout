e31170ffddbfc7cf5cb943917de886a493e4ee84
sunk3rn <kajakobliha@seznam.cz>
2023-11-22 23:33:26 +0100

    added exit function, cleaned up

--- hra.c
+++ hra.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 
 void game_init (SDL_Window ** window, SDL_Renderer ** renderer) {
+    SDL_Init(SDL_INIT_VIDEO);   // Inicializace SDL
     // Vytvoření okna
     *window = SDL_CreateWindow(
         "Breakout",  // Název
@@ -19,19 +20,23 @@ void game_init (SDL_Window ** window, SDL_Renderer ** renderer) {
     );
 }
 
+void quit_game(SDL_Window ** window, SDL_Renderer ** renderer) {
+    // Uvolnění prostředků
+    SDL_DestroyRenderer(*renderer);
+    SDL_DestroyWindow(*window);
+    SDL_Quit();
+}
+
 int main()
 {
-    bool keyboard = true;
-    SDL_Init(SDL_INIT_VIDEO);   // Inicializace SDL
-
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
     game_init(&window,&renderer);
 
     SDL_Event event;
     bool quit = false;
-
-    int mouse_x = 400;
+    bool keyboard = true;
+    int pos_x = 400;
 
     while (!quit)
     {
@@ -44,21 +49,21 @@ int main()
                 quit = true;
             }
             if(event.type == SDL_MOUSEMOTION && !keyboard) {
-                mouse_x =  event.motion.x;
-                if (mouse_x-50 <= 50) mouse_x = 80;
-                else if (mouse_x+50 >= 750) mouse_x = 720;
+                pos_x =  event.motion.x;
+                if (pos_x-50 <= 50) pos_x = 80;
+                else if (pos_x+50 >= 750) pos_x = 720;
             }
             if (SDL_KEYDOWN && keyboard) {
                 switch (event.key.keysym.sym)
                 {
                 case SDLK_LEFT:
-                    mouse_x -=  10;
-                    if (mouse_x-50 <= 20) mouse_x = 80;
+                    pos_x -=  10;
+                    if (pos_x-50 <= 20) pos_x = 80;
                     break;
                 
                 case SDLK_RIGHT:
-                    mouse_x +=  10;
-                    if (mouse_x+50 >= 780) mouse_x = 720;
+                    pos_x +=  10;
+                    if (pos_x+50 >= 780) pos_x = 720;
                     break;
                 
                 default:
@@ -76,16 +81,14 @@ int main()
         // Nastavení barvy na červenou
         SDL_SetRenderDrawColor(renderer, 255, 128, 128, 255);
 
-        SDL_Rect rectToDraw = {mouse_x-50,550,100,20}; //x_start, y_start, width, height
+        SDL_Rect rectToDraw = {pos_x-50,550,100,20}; //x_start, y_start, width, height
         SDL_RenderFillRect(renderer, &rectToDraw);
 
         // Zobrazení vykreslených prvků na obrazovku
         SDL_RenderPresent(renderer);
     }
-    // Uvolnění prostředků
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+
+    quit_game(&window,&renderer);
 
     return 0;
 }
