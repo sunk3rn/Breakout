93ef2b77cf6a1bb01da0a159a5e6714467ec8a48
sunk3rn <kajakobliha@seznam.cz>
2023-12-23 21:56:16 +0100

    Ball moves more naturally. Added first blocks

--- hra.c
+++ hra.c
@@ -10,7 +10,15 @@ typedef struct {
 
 void game_init (SDL_Window ** window, SDL_Renderer ** renderer, int width, int height) {
     SDL_Init(SDL_INIT_VIDEO);   // Inicializace SDL	
-    TTF_Init();
+
+    if(TTF_Init() == -1) {
+        printf( "Unable to render text surface! SDL2_ttf Error: %s\n", TTF_GetError() );
+        exit(1);
+    }
+    else {
+        printf("SDL2_ttf working fine\n");
+    }
+
     // Vytvoření okna
     *window = SDL_CreateWindow(
         "Breakout",  // Název
@@ -37,6 +45,7 @@ void quit_game(SDL_Window ** window, SDL_Renderer ** renderer) {
     TTF_Quit();
 }
 
+
 int main()
 {
     int res_width = 800;
@@ -44,14 +53,6 @@ int main()
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
     game_init(&window,&renderer,res_width,res_height);
-
-    if(TTF_Init() == -1) {
-        printf( "Unable to render text surface! SDL2_ttf Error: %s\n", TTF_GetError() );
-        exit(1);
-    }
-    else {
-        printf("SDL2_ttf working fine\n");
-    }
 
     TTF_Font* font = TTF_OpenFont("../assets/VT323-Regular.ttf",32);
     if(font == NULL) {
@@ -70,7 +71,8 @@ int main()
 
     SDL_Event event;
     bool quit = false;
-    bool keyboard = true;
+    bool keyboard = false;
+    
     int pos_x = 400;
     bool movement = false;
     int ball_x = 400;
@@ -78,6 +80,15 @@ int main()
     Ball ball = {.texture.x = ball_x, .texture.y = ball_y, .texture.w = 10, .texture.h = 10, .dir_x = 1, .dir_y = -1};
     Ball hitbox_ball = {.texture.x = ball_x -1, .texture.y = ball_y -1, .texture.w = 12, .texture.h = 12, .dir_x = 1, .dir_y = 1};
     bool fly_flag = true;
+
+    SDL_Rect * green_blocks = (SDL_Rect *) malloc (sizeof(SDL_Rect) * 15);
+
+    for (int i = 0;i < 15;i++) { 
+        green_blocks[i].x = i * 50 + 25;
+        green_blocks[i].y = 100;
+        green_blocks[i].w = 50;
+        green_blocks[i].h = 20;
+    }
 
     while (!quit)
     {
@@ -134,6 +145,19 @@ int main()
         SDL_Rect topBound = {20,40,res_width - 40,20};
         SDL_RenderFillRect(renderer,&topBound);
 
+        //Vykreslování bloků
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        for (int i = 0;i < 15;i++) { 
+            SDL_RenderFillRect(renderer, &green_blocks[i]);
+        }
+
+        // SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        // for (int i = 0;i < 15;i++) { 
+        //     SDL_Rect block = {i * 50 + 25,120,50,20};
+        //     SDL_RenderFillRect(renderer, &block);
+        // }
+
+
         // Vykreslení hráčské pálky
         SDL_SetRenderDrawColor(renderer, 255, 128, 128, 255);
         SDL_Rect paddle = {pos_x-50,550,100,20}; //x_start, y_start, width, height
@@ -142,28 +166,42 @@ int main()
 
         
         //BALL RENDERING
-
-        ball.texture.x = ball_x;
-        hitbox_ball.texture.x = ball_x -1;
-
         if (ball_y >= 590) {
                 fly_flag = false;
         }
 
         if (fly_flag) {
-            if ( ball_x + ball.texture.w >= res_width - 20 || ball_x <= 20 || (SDL_HasIntersection(&hitbox_ball.texture, &paddle) && (ball_y + 10 < 550))) {
+
+            //Odrážení míčku od vrchu pálky
+            if (SDL_HasIntersection(&hitbox_ball.texture, &paddle) && (ball_y < 550)) {
+                printf("dirx: %d,diry:%d\n",ball.dir_x, ball.dir_y);
+                if (ball.dir_x == -1 && ball.dir_y == 1 ) {
+                    ball.dir_y = -1;
+                    hitbox_ball.dir_y = -1;
+                }
+                else if (ball.dir_x == 1 && ball.dir_y == 1 ) {
+                    ball.dir_y = -1;
+                    hitbox_ball.dir_y = -1;
+                }
+                printf("AFTER dirx: %d,diry:%d\n",ball.dir_x, ball.dir_y);
+            }
+
+
+            //Kontrola kolize s bočními hranicemi herního pole
+            if ( ball_x + ball.texture.w >= res_width - 20 || ball_x <= 20) {
                 ball.dir_x *= -1;
                 hitbox_ball.dir_x *= -1;
             }
-            
 
-            else if ( ball_y + ball.texture.h >= res_height || ball_y <= 60 || (SDL_HasIntersection(&hitbox_ball.texture, &paddle) && (ball_y + 10 < 550))) {
+            if ( ball_y + ball.texture.h >= res_height || ball_y <= 60 ) {
                 ball.dir_y *= -1;
                 hitbox_ball.dir_y *= -1;
             }
 
-            if (SDL_HasIntersection(&hitbox_ball.texture, &paddle) && (ball_y + 10> 550)) {
+            //Boční odrážení míčku od pálky 
+            if (SDL_HasIntersection(&hitbox_ball.texture, &paddle) && (ball_y > 550)) { 
                 printf("texture_y: %d,ball_y:%d\n",hitbox_ball.texture.y, ball_y);
+                printf("dirx: %d,diry:%d\n",ball.dir_x, ball.dir_y);
                 if (SDL_HasIntersection(&hitbox_ball.texture, &paddle)) {
                     ball_y -= ((ball_y + ball.texture.h) - 550 + 2) ; 
                 }
@@ -172,27 +210,55 @@ int main()
                 hitbox_ball.dir_x *= -1;
                 ball.dir_y *= -1;
                 hitbox_ball.dir_y *= -1;
+                printf("AFTER dirx: %d,diry:%d\n",ball.dir_x, ball.dir_y);
             }
 
+            //Kontrola kolize s bloky
+            for (int i = 0;i < 15;i++) {
+                if (SDL_HasIntersection(&hitbox_ball.texture, &green_blocks[i])) {
+                    if (ball.dir_x == 1 && ball.dir_y == -1 ) {
+                        ball.dir_y = 1;
+                        hitbox_ball.dir_y = 1;
+                    }
+                    else if (ball.dir_x == -1 && ball.dir_y == -1 ) {
+                        ball.dir_y = 1;
+                        hitbox_ball.dir_y = 1;
+                    }
+                    // else if (ball.dir_x == 1 && ball.dir_y == -1 ) {
+                    //     ball.dir_y = 1;
+                    //     hitbox_ball.dir_y = 1;
+                    // }
+                    // else if (ball.dir_x == -1 && ball.dir_y == 1 ) {
+                    //     ball.dir_y = 1;
+                    //     hitbox_ball.dir_y = 1;
+                    // }
+
+                }
+                
+            }
+
+            //Pohyb míčku
+            ball.texture.x = ball_x;
+            hitbox_ball.texture.x = ball_x -1;
             ball_x += ball.dir_x * 3;
 
             ball.texture.y = ball_y;
             hitbox_ball.texture.y = ball_y -1;
             ball_y += ball.dir_y * 3;
+
+            
         }
         
-        
-
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
         SDL_RenderFillRect(renderer,&hitbox_ball.texture);
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_RenderFillRect(renderer,&ball.texture);
 
-
-
         // Zobrazení vykreslených prvků na obrazovku
         SDL_RenderPresent(renderer);
     }
+
+    free(green_blocks);
 
     quit_game(&window,&renderer);
 
